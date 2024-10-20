92b9ea6c8dbbd4792f34d39ff7e4a6ea9218fb38
Harsh Gandhi <123467354+HG1210@users.noreply.github.com>
2024-10-20 11:53:31 +0530

    Add files via upload

---
+++ Fun.py
@@ -0,0 +1,28 @@
+def greater(a,b):
+    if(a>b):
+        print("The number a is greater than b")
+    else:
+        print("The number is b is greater than a")
+
+def lesser(a,b):
+    if(a<b):
+        print("Hehe a is smaller than b")
+    else:
+        print("Hehe b is smaller than a")
+
+
+a = int(input("Enter the number"))
+b = int(input("Enter the number"))
+greater(a, b)
+lesser(a,b)
+
+
+def LearnPy(name):
+    if(enjoy == 'Weekend'or 'weekend'):
+        print('Learn Python ')
+        print('Build a website using python')
+    else:
+        print('Go to Office & work')
+
+enjoy = input("Enter the string:")
+LearnPy(enjoy)
